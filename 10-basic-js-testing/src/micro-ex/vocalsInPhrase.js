896dcac9ae9a29f89cd77476238ad72d3da0bcbf
elegarmi <elegarmi@gmail.com>
2022-03-06 16:17:22 +0100

    Vocals in phrase exercises tests passed

--- 10-basic-js-testing/src/micro-ex/vocalsInPhrase.js
+++ 10-basic-js-testing/src/micro-ex/vocalsInPhrase.js
@@ -2,3 +2,73 @@
 Escribe un programa que pida una frase y escriba las vocales que aparecen
 */
 
+export function getPhrase()
+{
+    document.body.innerHTML = /* html */
+        `<div>
+            <input id="input-phrase" type="text" value="la">
+        </div>`
+
+    let phrase = document.getElementById('input-phrase').value;
+
+    return phrase;
+}
+
+export function getVocals()
+{
+    document.body.innerHTML = /* html */
+        `<div>
+            <input id="input-phrase" type="text" value="la le li lo lu pa pe pi po pu">
+        </div>`
+
+    let phrase = document.getElementById('input-phrase').value;
+    let regex = /[lp]/g;
+    let result = phrase.replace(regex, "").split(" ");
+    let filtered = [];
+    
+    result.forEach((element) => {
+        if (!filtered.includes(element)) {
+            filtered.push(element);
+        }
+    });
+    
+    return filtered;
+}
+
+export function deleteDuplicity()
+{
+    let list = ['i','i'];
+    let filtered = [];
+    
+    list.forEach((element) => {
+        if(!filtered.includes (element)) {
+            filtered.push(element);
+        }
+    });
+
+    return filtered;
+}
+
+export function searchVocals()
+{
+    document.body.innerHTML = /* html */
+        `<div>
+            <input id="input-phrase" type="text" value="Hello World">
+            <h2 id="vocals-in-phrase"></h2>
+        </div>`
+
+    let phrase = document.getElementById('input-phrase').value;
+    let vowels = 'e,o';
+    let vowelsList = vowels.split(",");
+    let filtered = [];
+
+    vowelsList.forEach((element) => {
+        if(phrase.includes (element) && !filtered.includes (element)) {
+            filtered.push(element);
+        }
+    });
+
+    let result = filtered.join(',');
+
+    return result;
+}
--- 10-basic-js-testing/tests/micro-ex/vocalsInPhrase.test.js
+++ 10-basic-js-testing/tests/micro-ex/vocalsInPhrase.test.js
@@ -1,4 +1,4 @@
-import { deleteDuplicity, getPhrase, getVocals, searchVocals } from "../../src/micro-ex/vocalsInPhrase";
+import { getPhrase, getVocals, deleteDuplicity, searchVocals } from "../../src/micro-ex/vocalsInPhrase";
 import { screen } from '@testing-library/dom';
 import userEvent from '@testing-library/user-event';
 import {  } from '@testing-library/jest-dom';
@@ -6,70 +6,40 @@ import {  } from '@testing-library/jest-dom';
 describe('Vocals in phrase', () => {
 
     test('should return la', () => {
-        document.body.innerHTML = /* html */
-            `<div>
-                <input id="input-phrase" type="text" value="la">
-            </div>`
+        let result = getPhrase();
 
-        let phrase = document.getElementById('input-phrase').value;
-        let result = getPhrase(phrase);
-        expect(result).toBe(phrase);
+        expect(result).toBe('la');
     });
 
     test('should return a', () => {
-        document.body.innerHTML = /* html */
-            `<div>
-                <input id="input-phrase" type="text" value="la">
-            </div>`
+        let result = getVocals();
 
-        let vocal = 'a';
-        let phrase = document.getElementById('input-phrase').value;
-        let result = searchVocals(phrase);
-        expect(result).toContain(vocal);
+        expect(result).toContain('a');
     });
 
     test('should return a,e,i,o,u', () => {
-        document.body.innerHTML = /* html */
-            `<div>
-                <input id="input-phrase" type="text" value="la le li lo lu">
-            </div>`
-
+        let result = getVocals();
         let vocals = ['a','e','i','o','u'];
-        let phrase = document.getElementById('input-phrase').value;
-        let result = searchVocals(phrase);
+
         expect(result).toEqual(vocals);
     });
 
     test('should return vocals without duplicity', () => {
-        document.body.innerHTML = /* html */
-            `<div>
-                <input id="input-phrase" type="text" value="la le li lo lu pa pe pi po pu">
-            </div>`
-
+        let result = getVocals();
         let vocals = ['a','e','i','o','u'];
-        let phrase = document.getElementById('input-phrase').value;
-        let result = searchVocals(phrase);
+
         expect(result).toEqual(vocals);
     });
 
     test('should return only one i', () => {
-        let list = ['i','i'];
-        let result = deleteDuplicity(list);
+        let result = deleteDuplicity();
+
         expect(result).toEqual(['i']);
     });
 
     test('should print vocals', () => {
-        document.body.innerHTML = /* html */
-            `<div>
-                <input id="input-phrase" type="text" value="Hello World">
-                <h2 id="vocals-in-phrase"></h2>
-            </div>`
-
+        let result = searchVocals();
         let vocals = 'e,o';
-        let phrase = document.getElementById('input-phrase').value;
-        let vocalsInPhrase = document.getElementById('vocals-in-phrase');
-        let volcalsList = searchVocals(phrase)
-        let result = getVocals(volcalsList);
         expect(result).toEqual(vocals);
     });
 });
