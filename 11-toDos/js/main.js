f474af43a7d1cb9fa541796ae677ebaf002fb11a
elegarmi <elegarmi@gmail.com>
2022-03-20 01:31:27 +0100

    Refact array to array of objects

--- 11-toDos/js/main.js
+++ 11-toDos/js/main.js
@@ -1,4 +1,5 @@
 let favouriteSongs = ["Welcome to wherever you are", "Waitin' on a sunny day", "Todo se transforma", "Mojado", "Hoppipolla", "Volaré", "Je veux", "Creep", "Una canción para la Magdalena", "Dies Irae"];
+favouriteSongs = favouriteSongs.map((name, index) => ({ id: index + 1, name: name}));
 
 let songsListContainer = document.getElementById("songslist-container");
 let inputNewSong = document.getElementById("in-new-song");
@@ -9,22 +10,22 @@ let btnTrash = document.getElementById("btn-trash");
 inputNewSong.placeholder = 'Song title';
 
 function renderListSongs() {
+
+    console.log(favouriteSongs);
+
     songsListContainer.innerHTML = '<ul id="songslist"></ul>'
     let songsList = document.getElementById("songslist");
 
     favouriteSongs.forEach(song => {
-        let songWithoutAccent = song.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-        let songLowerWithoutSpaces = songWithoutAccent.toLowerCase().replace(/\s|["']/g, '');
-        let songId = songLowerWithoutSpaces +"-"+ favouriteSongs.indexOf(song);
 
-        let songEdit = `<i class="fa fa-edit" id="btn-Edit" onclick="editSong('${songId}')"></i>`;
-        let songDelete = `<i class="fa fa-trash-o" id="btn-Trash" onclick="deleteSong('${songId}')"></i>`;
+        let songEdit = `<i class="fa fa-edit" id="btn-Edit" onclick="editSong(${song.id})"></i>`;
+        let songDelete = `<i class="fa fa-trash-o" id="btn-Trash" onclick="deleteSong(${song.id})"></i>`;
         
         let songButtons = songEdit + songDelete;
 
-        songsList.innerHTML += `<li id="${songId}"><span>${song}</span><div class="fa-icons">${songButtons}</div></li>`;
-        });
+        songsList.innerHTML += `<li id="${song.id}"><span>${song.name}</span><div class="fa-icons">${songButtons}</div></li>`;
     
+    });
 }
 
 renderListSongs();
@@ -40,37 +41,48 @@ inputNewSong.addEventListener('keyup', function (event) {
 function addSong() {
     if(inputNewSong.value != '') {
 
-        let songName = inputNewSong.value;
+        let newSongName = inputNewSong.value;
+        let lastSong = favouriteSongs.slice(-1);
+        let song = { id: lastSong[0].id + 1, name: newSongName };
 
-        if(favouriteSongs.includes(inputNewSong.value)) {
-            alert(songName + " is already on the list");
+        if(favouriteSongs.includes(newSongName)) {
+            alert(newSongName + " is already on the list");
         }
 
         else {
-            favouriteSongs.push(songName);
+            favouriteSongs.push(song);
         }
 
-        inputNewSong.value = '';
+        newSongName = '';
         renderListSongs();
     }
 
     if (inputNewSong.value = '') {
         inputNewSong.placeholder = 'Add a song!';
+        alert("Add a song!");
     }
 
 }
 
 function editSong(id) {
-    let songTitle = document.getElementById(id).childNodes[0].innerHTML;
-    inputNewSong.value = songTitle;
+    let chosenElement = document.getElementById(id);
+    let songTitle = chosenElement.childNodes[0].innerHTML;
+    let foundSong = favouriteSongs.find(song => song.name === songTitle);
+
+    if(foundSong.name === songTitle) {
+        inputNewSong.value = foundSong.name;
+    }
 }
 
 function deleteSong(id) {
-    let idElements = id.split("-");
-    let songPosition = idElements[1];
+    let chosenElement = document.getElementById(id);
+    let songTitle = chosenElement.childNodes[0].innerHTML;
 
-    favouriteSongs.splice(songPosition, 1);
-    document.getElementById(id).remove();
+    chosenElement.remove();
+
+    favouriteSongs = favouriteSongs.filter(function(element) {
+        return element.name !== songTitle;
+    });
 
     renderListSongs();
 }
